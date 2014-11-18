5c340818a785f4d67f4d0477aaf586211fc18f9d
ArekJ <arek.juszczyk@ed.ac.uk>
2014-11-18 11:32:24 +0100

    nowy wymiar twb logo

--- adres_zg.js
+++ adres_zg.js
@@ -1,5 +1,5 @@
 document.write('<p>tel. +48 728 804 621</p>\
 <p>ul. Kazimierza Wielkiego 7/5<br/>\
 65-047 Zielona Góra</p>\
-<a href="http://twbiuro.pl/index.html" target="_blank"><img src="/gfx/twb.png" width="135" height="43"></a>\
+<a href="http://twbiuro.pl/index.html" target="_blank"><img src="/gfx/twb.png" width="130" height="44"></a>\
 ');
