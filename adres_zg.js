fd01c4f362a58ebcb8dcf396abb529e0e5ae367e
ArekJ <arek.juszczyk@ed.ac.uk>
2014-11-18 11:40:57 +0100

    twb logo pozycja

--- adres_zg.js
+++ adres_zg.js
@@ -1,5 +1,5 @@
 document.write('<p>tel. +48 728 804 621</p>\
 <p>ul. Kazimierza Wielkiego 7/5<br/>\
 65-047 Zielona Góra</p>\
-<a href="http://twbiuro.pl/index.html" target="_blank"><img src="/gfx/twb.png" width="130" height="44"></a>\
+<div style="margin-left:10px;"><a href="http://twbiuro.pl/index.html" target="_blank"><img src="/gfx/twb.png" width="130" height="44"></a></div>\
 ');
